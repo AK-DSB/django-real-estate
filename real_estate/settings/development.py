6c15b5b569b3cf2d7d9d77ada59e420ec17bdaf5
AKW <2497744746@qq.com>
2023-05-24 16:15:59 +0800

    setup celery

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,13 +1,14 @@
 from .base import *
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_USE_TLS = True
 EMAIL_PORT = env('EMAIL_PORT')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'info@real_estate.com'
+DEFAULT_FROM_EMAIL = '2497744746@qq.com'
 DOMAIN = env('DOMAIN')
 SITE_NAME = 'Real Estate'
 
@@ -22,3 +23,7 @@ DATABASES = {
         'PORT': env('PG_PORT'),
     }
 }
+
+CELERY_BROKER_URL = env('CELERY_BROKER')
+CELERY_RESULT_BACKEND = env('CELERY_BACKEND')
+CELERY_TIMEZONE = 'Asia/Shanghai'
